c41a1da189549f5569445f30e181ec63633a90d2
Torfab <torfab94@gmail.com>
2024-01-28 10:49:23 +0100

    Banana può colpire se stesso

--- Scene/BulletsBanana.gd
+++ Scene/BulletsBanana.gd
@@ -16,6 +16,6 @@ func _on_area_2d_body_entered(body):
 	if body.is_in_group("Giocatore"):
 		if (!body.is_in_group("P"+str(shooter))):
 			global.punteggio[shooter-1] += global.sistemaPunti["Torta"] + randi_range(0,10)
-			body.scivola(true)
+		body.scivola(true)
 		queue_free()
 	pass # Replace with function body.
