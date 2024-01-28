aadef045710e5247c4c90cc89bd61e862499c283
Torfab <torfab94@gmail.com>
2024-01-28 13:11:59 +0100

    balancing note

--- Scripts/globals.gd
+++ Scripts/globals.gd
@@ -8,10 +8,10 @@ var punteggio = [0,0,0,0]
 
 
 var sistemaPunti = {
-	"Martello": 10,
-	"Fiore": 15,
-	"Torta": 20,
-	"Banana": 20
+	"Martello": 5,
+	"Fiore": 2,
+	"Torta": 50,
+	"Banana": 50
 }
 
 var weapons = [
